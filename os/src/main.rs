9be45174c661234be462cfb6c7203c0675073f62
leo <wangsihan_leo@sjtu.edu.cn>
2025-04-22 15:19:00 +0800

    finish ch1

---
+++ os/src/main.rs
@@ -0,0 +1,25 @@
+#![no_main]
+#![no_std]
+
+#[macro_use]
+mod console;
+mod lang_items;
+mod sbi;
+
+use core::arch::global_asm;
+global_asm!(include_str!("entry.asm"));
+
+#[unsafe(no_mangle)]
+pub fn rust_main() -> ! {
+    clear_bss();
+    println!("Hello, world!");
+    panic!("Shutdown machine!");
+}
+
+fn clear_bss() {
+    unsafe extern "C" {
+        unsafe fn sbss();
+        unsafe fn ebss();
+    }
+    (sbss as usize..ebss as usize).for_each(|a| unsafe { (a as *mut u8).write_volatile(0) });
+}
